a50b98f8f74ff67c902b0faa68e1feac434e3269
donna6355 <donnajun1214@gmail.com>
2024-03-05 13:31:13 +0900

    [add] constant and scanf_s

--- study_c/variable.c
+++ study_c/variable.c
@@ -11,5 +11,16 @@ int main() {
 
 	double d = 4.428;
 	printf("%.2lf\n", d);
+
+
+	const int YEAR = 1972;
+	printf("C has been published in %d\n", YEAR);
+
+	printf("%d + %d = %d\n", 3, 7, 3 + 7);
+
+	int num;
+	printf("Please input integer\n");
+	scanf_s("%d", &num);
+	printf("%d is what you typed\n", num);
 	return 0;
 }
