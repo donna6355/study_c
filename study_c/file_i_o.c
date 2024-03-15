18ae00f5269416dd134d2e8546098543e3b76555
donna6355 <donnajun1214@gmail.com>
2024-03-15 11:10:20 +0900

    [add] fprintf, fscanff

--- study_c/file_i_o.c
+++ study_c/file_i_o.c
@@ -1,9 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS
-#define MAX 10000
+#define MAX 10
 #include <stdio.h>
 
 int main()
 {
+  int count = 0;
   /*
   r : read only. the file must exist
   w : write only. if file doesn't exist, create new one, otherwise rewrite on the file
@@ -22,7 +23,7 @@ int main()
     return 1;
   }
   fputs("TGIF Isaac:)\n", textFile);
-  fputs("Is everything okay with you?\n", textFile);
+  fputs("Did you puke this morning? what happened?\n", textFile);
   fclose(textFile);
 
   textFile = fopen("C:\\Users\\donna\\Desktop\\test.txt", "rb");
@@ -36,7 +37,29 @@ int main()
   while (fgets(line, MAX, textFile) != NULL)
   {
     printf("%s", line);
+    printf("\n *****read file %d times*****\n", count++);
   }
+  fclose(textFile);
+
+  textFile = fopen("C:\\Users\\donna\\Desktop\\testTwo.txt", "wb");
+  if (textFile == NULL)
+  {
+    printf("FAILED TO OPEN WRITE FILE FOR FPRINTF\n");
+    return 1;
+  }
+  fprintf(textFile, "%s %d %d %d %d %d\n", "RANDOMNUMBER:", 6, 3, 5, 5, 2);
+  fprintf(textFile, "%s %d\n", "ISAAC NUMBER :", 1231);
+  fclose(textFile);
+  textFile = fopen("C:\\Users\\donna\\Desktop\\testTwo.txt", "rb");
+  if (textFile == NULL)
+  {
+    printf("FAILED TO OPEN READ FILE FOR FSCANF\n");
+    return 1;
+  }
+  int random[5];
+  char str[1000];
+  fscanf(textFile, "%s %d %d %d %d %d", str, &random[0], &random[1], &random[2], &random[3], &random[4]);
+  printf("this is what you got : %s %d,%d,%d\n",str,random[0],random[1],random[4]);
   fclose(textFile);
   return 0;
 }
